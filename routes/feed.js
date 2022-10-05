70a249ffa222a9c7f35868275c31a9d5624bb92d
azmammad.q <azmammad.q@kripton.az>
2022-10-05 16:16:05 +0400

    delete method added

--- routes/feed.js
+++ routes/feed.js
@@ -11,5 +11,6 @@ router.post(
   feedController.createPost
 );
 router.get("/post/:postId", feedController.getPost);
-router.put("/post/:postId",feedController.updatePost);
+router.put("/post/:postId", feedController.updatePost);
+router.delete("/post/:postId",feedController.deletePost);
 module.exports = router;
